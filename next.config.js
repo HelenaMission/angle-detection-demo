33d3dd6ddcaf976f68441ecf500f300e313d2ca6
Helena Lee <132238788+HelenaMission@users.noreply.github.com>
2023-11-20 16:12:46 +1300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    customVisionApiKey: '8b89ad75c8494a78a316eaa54e59f932',
+    customVisionApiKey: '',
     customVisionApiEndpoint:
-      'https://wateringcanangledetection-prediction.cognitiveservices.azure.com/customvision/v3.0/Prediction/93bdb3b2-3f81-4572-993b-f8068f49b62e/classify/iterations/object-angle-prediction/image',
+      '',
   },
 };
 
